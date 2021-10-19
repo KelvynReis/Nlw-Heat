37063f18bdcac232a1cf8ae722483175238085db
Kelvyn Reis <kelvyn2reis@gmail.com>
2021-10-19 15:21:41 -0300

    authenticate user

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -6,11 +6,15 @@ class AuthenticateUserController{
     async handle( request: Request, response: Response){
 
         const { code } = request.body;
-        
         const service = new AuthenticateUserService();
-        const result = await service.execute(code)
 
-        return response.json(result)
+        try {
+            const result = await service.execute(code)
+            return response.json(result)
+
+        } catch (err) {
+            return response.json(err.message)
+        }
     }
 }
 
